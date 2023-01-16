aa90ffcf053bc907e4091fae079cbfaa740ac45f
Benjamin Carpentier <benjamincarpentier01@gmail.com>
2023-01-16 09:44:42 +0100

    feat: modification de la tache

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -934,54 +934,96 @@ const ulContainer = document.querySelector('ul');
 const form = document.querySelector("form");
 const input = document.querySelector("form > input");
 const todos = [{
-  text: "aller faire les courses",
-  done: false
+  text: "Aller faire les courses",
+  done: false,
+  editMode: false
 }, {
-  text: "aller chercher les résultats",
-  done: true
+  text: "Aller chercher les resultats cliniques",
+  done: true,
+  editMode: false
 }];
 form.addEventListener('submit', event => {
   event.preventDefault(); // on arrete le rechargement de la page
-  const todoText = input.value; // je récupere le texte du input zone saisie
-  input.value = ''; // reinitialise la zone de saisie
+  const todoText = input.value; // je recupere le texte dans le zone de saisie du input
+  input.value = ''; // on reinitialise la zone de saisie en la vidant
 
   const newTodo = {
     text: todoText,
-    done: false
-  };
-  todos.push(newTodo); // on ajoute la tache dans la liste des taches
+    done: false,
+    editMode: false
+  }; // on creer une tache
+  todos.push(newTodo); // on ajoute cette nouvelle tache dans la liste des taches todos
   displayTodos();
 });
 
-//fonction fléchée qui va creer et remplir une balise <li>
+// fonction flechee qui va creer et remplir une balise <li>
 const createTodoElement = (todo, index) => {
-  const li = document.createElement(`li`);
+  const li = document.createElement('li');
   const deleteBtn = document.createElement('button');
+  const editBtn = document.createElement('button');
   li.classList.add("d-flex", "align-items-start");
   deleteBtn.classList.add("btn", "btn-danger", "mx-2");
-  deleteBtn.innerText = "Supprimer";
+  editBtn.classList.add("btn", "btn-primary", "mx-2");
+  deleteBtn.innerText = 'Supprimer';
   deleteBtn.addEventListener('click', event => {
-    event.stopPropagation();
+    event.stopPropagation(); // on arrete la propagation de l'event
     todos.splice(index, 1);
+    displayTodos();
+  });
+  editBtn.innerText = 'Editer';
+  editBtn.addEventListener('click', event => {
+    event.stopPropagation();
+    todos[index].editMode = !todos[index].editMode;
     displayTodos();
   });
   li.innerHTML = `
         <span class="todo ${todo.done ? 'done' : ''}"></span>
         <p class="w-100">${todo.text}</p>
-        <button class="btn btn-primary mx-2">Editer</button>
     `;
   li.addEventListener('click', event => {
-    todos[index].done = !todos[index].done;
+    todos[index].done = !todos[index].done; // != different de
     displayTodos();
   });
-  li.appendChild(deleteBtn);
+  li.append(editBtn, deleteBtn);
+  return li;
+};
+const createTodoEditElement = (todo, index) => {
+  const li = document.createElement('li');
+  const input = document.createElement('input');
+  const saveBtn = document.createElement('button');
+  const cancelBtn = document.createElement('button');
+  li.classList.add("d-flex", "align-items-start");
+  input.classList.add("w-100");
+  saveBtn.classList.add("btn", "btn-primary", "mx-2");
+  cancelBtn.classList.add("btn", "btn-danger", "mx-2");
+  input.type = "text";
+  input.value = todo.text;
+  saveBtn.innerText = "Sauvegarder";
+  cancelBtn.innerText = "Annuler";
+  cancelBtn.addEventListener('click', event => {
+    event.stopPropagation();
+    todos[index].editMode = !todos[index].editMode;
+    displayTodos();
+  });
+  saveBtn.addEventListener('click', event => {
+    event.stopPropagation();
+    const value = input.value;
+    todos[index].text = value;
+    todos[index].editMode = false;
+    displayTodos();
+  });
+  li.append(input, cancelBtn, saveBtn);
   return li;
 };
 
-// fonction fléchée qui va afficher la liste des taches, map => array, filter => array, reduce => valeur
+// fonction flechee qui va afficher la liste des taches, map => array, filter => array, reduce => valeur
 const displayTodos = () => {
   const todosNode = todos.map((todo, index) => {
-    return createTodoElement(todo, index);
+    if (todo.editMode) {
+      return createTodoEditElement(todo, index);
+    } else {
+      return createTodoElement(todo, index);
+    }
   });
   ulContainer.innerHTML = '';
   ulContainer.append(...todosNode);
